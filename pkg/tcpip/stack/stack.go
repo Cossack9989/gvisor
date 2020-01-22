c1718e19d74182b836918eb578580aad643827cd
Ian Gudger <igudger@google.com>
2020-01-22 13:46:42 -0800

    Add support for tearing down protocol dispatchers and TIME_WAIT endpoints.
    Protocol dispatchers were previously leaked. Bypassing TIME_WAIT is required to
    test this change.
    
    Also fix a race when a socket in SYN-RCVD is closed. This is also required to
    test this change.
    
    PiperOrigin-RevId: 291021977

--- pkg/tcpip/stack/stack.go
+++ pkg/tcpip/stack/stack.go
@@ -1407,7 +1407,13 @@ func (s *Stack) RestoreCleanupEndpoints(es []TransportEndpoint) {
 // Endpoints created or modified during this call may not get closed.
 func (s *Stack) Close() {
 	for _, e := range s.RegisteredEndpoints() {
-		e.Close()
+		e.Abort()
+	}
+	for _, p := range s.transportProtocols {
+		p.proto.Close()
+	}
+	for _, p := range s.networkProtocols {
+		p.Close()
 	}
 }
 
@@ -1424,6 +1430,12 @@ func (s *Stack) Wait() {
 	}
 	for _, e := range s.CleanupEndpoints() {
 		e.Wait()
+	}
+	for _, p := range s.transportProtocols {
+		p.proto.Wait()
+	}
+	for _, p := range s.networkProtocols {
+		p.Wait()
 	}
 
 	s.mu.RLock()
